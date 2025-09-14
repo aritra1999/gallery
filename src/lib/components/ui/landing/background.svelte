daf82f66163012aea0874f40615598b3c319bb01
Aritra Mondal <aritra041999.dev@gmail.com>
2025-09-14 22:36:24 +0200

    feat: add background component for landing page

---
+++ src/lib/components/ui/landing/background.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	let { label } = $props();
+</script>
+
+<div
+	class="fixed right-0 bottom-0 -z-100 w-screen text-right text-[20vw] leading-[18vw] font-bold tracking-tighter text-muted md:text-[15vw] md:leading-[12vw]"
+>
+	{label}
+</div>
