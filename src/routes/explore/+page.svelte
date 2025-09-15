52534d7282874ec60818f7005adeb1a82650e189
Aritra Mondal <aritra041999.dev@gmail.com>
2025-09-15 13:39:09 +0200

    feat: load images in the background

--- src/routes/explore/+page.svelte
+++ src/routes/explore/+page.svelte
@@ -3,6 +3,7 @@
 	import { Button } from '$lib/components/ui/button';
 	import * as Dialog from '$lib/components/ui/dialog/index.js';
 	import Background from '$lib/components/ui/landing/background.svelte';
+	import { onMount } from 'svelte';
 
 	let { data } = $props();
 	let page = $state(1);
@@ -10,19 +11,29 @@
 	let assets = $state<Asset[]>(data.assets || []);
 	let open = $state(false);
 	let selectedAsset = $state<Asset | null>(null);
+	let noMoreAssets = $state(false);
+	let showLoadingModal = $state(true);
 
 	async function loadNextPage() {
-		console.log('Function called ');
 		loading = true;
 		await fetch(`/assets?t=${data.tag}&page=${++page}`)
 			.then((res) => res.json())
-			.then((body) => assets.push(...(body as Asset[])))
+			.then((body) => {
+				if (body.length === 0) noMoreAssets = true;
+				assets.push(...(body as Asset[]));
+			})
 			.catch((err) => {
 				console.log(err);
 			});
 
 		loading = false;
 	}
+
+	onMount(() => {
+		setTimeout(() => {
+			showLoadingModal = false;
+		}, 3000);
+	});
 </script>
 
 <svelte:head>
@@ -35,7 +46,13 @@
 		{data.tag}
 	</h1>
 </div>
-
+{#if showLoadingModal}
+	<div
+		class="fixed top-16 left-0 z-50 flex h-[calc(100vh-4rem)] w-screen items-center justify-center bg-background"
+	>
+		Loading assets...
+	</div>
+{/if}
 <div>
 	{#if assets && assets.length > 0}
 		<div class="columns-2 gap-2 md:columns-3 lg:columns-4 xl:columns-5">
@@ -60,7 +77,6 @@
 							<img
 								src={asset.url}
 								alt={asset.url}
-								loading="lazy"
 								class="z-20 w-full rounded-lg object-cover transition-transform duration-300 group-hover:scale-105"
 							/>
 						{/if}
@@ -77,15 +93,17 @@
 				</button>
 			{/each}
 		</div>
-		<div class="z-20 mt-10 mb-96 flex justify-center">
-			<Button class="z-20" variant="outline" onclick={loadNextPage} disabled={loading}>
-				{#if loading}
-					Loading...
-				{:else}
-					Load More
-				{/if}
-			</Button>
-		</div>
+		{#if !noMoreAssets}
+			<div class="z-20 mt-10 mb-96 flex justify-center">
+				<Button class="z-20" variant="outline" onclick={loadNextPage} disabled={loading}>
+					{#if loading}
+						Loading...
+					{:else}
+						Load More
+					{/if}
+				</Button>
+			</div>
+		{/if}
 	{:else}
 		<p class="mt-20 text-center">No assets found.</p>
 	{/if}
@@ -97,6 +115,7 @@
 			{#if selectedAsset.mimeType.startsWith('video/')}
 				<video
 					controls={true}
+					autoplay={true}
 					class="max-h-[calc(100vh-2rem)] max-w-[calc(100vw-2rem)] rounded-lg object-contain md:max-h-[calc(100vh-4rem)] md:max-w-[calc(100vw-4rem)]"
 				>
 					<source src={selectedAsset.url} type={selectedAsset.mimeType} />
